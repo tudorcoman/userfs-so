96cfc010c3663bf4382e8a8b6dc0cf02dbe4f614
wood11nho <93677864+wood11nho@users.noreply.github.com>
2023-01-18 01:33:31 +0200

    Documentatie + comentarii (#3)
    * documentatie plus comentarii
    
    * final version

--- active_users.c
+++ active_users.c
@@ -6,16 +6,17 @@
 #include <pthread.h>
 
 ActiveUserList get_active_users() {
+    
     char* command = "who | awk '{print $1}' | sort -u";
     char data[MAX_CHARS_USERNAME]; // presupunem ca un nume de user are maxim 512 caractere
 
     FILE *pipe_output_comanda = open_pipe(command);
 
-    // Error handling
+    //Tratare erori
 
     ActiveUserList x;
     int i = 0;
-    // Get the data from the process execution
+
     while(fgets(data, MAX_CHARS_USERNAME, pipe_output_comanda)) {
         // in data avem fiecare user
         x.active_users[i] = (char*) malloc(strlen(data) * sizeof(char));
@@ -28,18 +29,19 @@ ActiveUserList get_active_users() {
     if (pclose(pipe_output_comanda) != 0)
         fprintf(stderr," Error: Failed to close command stream \n");
 
-    x.count = i;
+    x.count = i; // numarul de useri activi
     return x;
 }
 
 void update_users(ActiveUserList* users) {
+
+    // actualizeaza lista de useri activi
     ActiveUserList x = get_active_users();
     users->count = x.count;
     int i;
     for (i = 0; i < x.count; i ++)
-	users->active_users[i] = x.active_users[i];
+        users->active_users[i] = x.active_users[i];
     for (i = x.count + 1; i < MAX_LOGGED_IN_USERS; i ++)
-	users->active_users[i] = NULL;
+        users->active_users[i] = NULL;
+
 }
-
-
--- active_users.h
+++ active_users.h
@@ -4,11 +4,11 @@
 #include "utils.h"
 
 typedef struct ActiveUserList{
-    char* active_users[MAX_LOGGED_IN_USERS];
-    int count;
+    char* active_users[MAX_LOGGED_IN_USERS]; // lista de useri activi
+    int count; // numarul de useri activi
 } ActiveUserList;
 
-ActiveUserList get_active_users();
-void update_users(ActiveUserList* users);
+ActiveUserList get_active_users(); // returneaza lista de useri activi
+void update_users(ActiveUserList* users); // actualizeaza lista de useri activi
 
 #endif //ACTIVE_USERS_H
